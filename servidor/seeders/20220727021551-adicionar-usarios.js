d6f85772eca84e31b0131f3a7a3ff5f274327401
izabella dos santos <izabella.miranda@estudante.ifms.edu.br>
2023-10-27 16:51:40 -0400

    commit css

--- servidor/seeders/20220727021551-adicionar-usarios.js
+++ servidor/seeders/20220727021551-adicionar-usarios.js
@@ -3,8 +3,7 @@ const crypto = require('../crypto')
 module.exports = {
   async up (queryInterface, Sequelize) {
     await queryInterface.bulkInsert('usuarios', [
-      { nome: 'John Doe', senha: '123' },
-      { nome: 'Picolo', senha: '123' },
+      { nome: 'John Doe', senha: crypto.encrypt('123') },
       { nome: 'Picolo', senha: crypto.encrypt('123') }
     ], {});
   },
